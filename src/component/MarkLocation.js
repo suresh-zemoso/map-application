6e513350e323a8aea3e97d365255ccb06420e577
Suresh Choudhary <suresh.choudhary@zemosolabs.com>
2020-04-21 16:14:20 +0530

    Create Mark Location page

---
+++ src/component/MarkLocation.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import BaseMapView from './BaseMapView';
+import mapboxgl from 'mapbox-gl';
+import { useParams } from 'react-router-dom';
+import { useSelector } from 'react-redux';
+
+
+export default function MarkLocation(props) {
+
+    const { id } = useParams();
+    const locations = useSelector(state => state.locations);
+    const mapAttribute = locations.find(location => {
+        return location.id == id
+    })
+    function flyToLocation(map) {
+        map.on('load', (e) => {
+            map.flyTo({
+                center: [
+                    mapAttribute.lng,
+                    mapAttribute.lat,
+                ],
+                zoom: 7,
+                essential: true // this animation is considered essential with respect to prefers-reduced-motion
+            });
+
+            var marker = new mapboxgl.Marker();
+            // Update the data to a new position based on the animation timestamp. The
+            // divisor in the expression `timestamp / 1000` controls the animation speed.
+            marker.setLngLat([
+                mapAttribute.lng,
+                mapAttribute.lat
+            ]);
+
+            // Ensure it's added to the map. This is safe to call if it's already added.
+            marker.addTo(map);
+
+            // Request the next frame of the animation.
+            // requestAnimationFrame(animateMarker);
+        });
+    }
+
+
+
+    return (
+        <BaseMapView mapAttribute={{ lng: 5, lat: 34, zoom: 2 }} handlers={[flyToLocation]} />
+    )
+}
